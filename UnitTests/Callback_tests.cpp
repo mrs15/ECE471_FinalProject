e98da6a8734b78da270435dca44064dbed8339c7
mrs15 <78612919+mrs15@users.noreply.github.com>
2023-03-25 14:31:19 -0400

    Add files via upload

--- UnitTests/Callback_tests.cpp
+++ UnitTests/Callback_tests.cpp
@@ -6,13 +6,13 @@
 
 // flag to see if the callback function was called or not
 static int called_flag = 0;
-static int twenty_fiveFlag = 0;
+static int twenty_seven_called_flag = 0;
 U8 ret;
 
 static void setup(void)
 {
     set_state(INIT_STATE);
-    Callbacks_Reset_Counter();
+    Callbacks_Init();
     ret = 1;
 }
 
@@ -23,188 +23,177 @@ static void FakeRTI(void)
 
 void ten_second_cb(void)
 {
-    set_state(WATER_PLANTS);
     called_flag += 1;
 }
 
-void twentyFiveSecondCB(void)
+void twenty_seven_sec_cb(void)
 {
-    twenty_fiveFlag += 1;
+    twenty_seven_called_flag += 1;
 }
 
-void cb1(void)
+static void CallRTI(int numTimes)
 {
-    twenty_fiveFlag += 1;
-}
-
-TEST(CallbackTests, ShallNotRegisterNullCallback)
-{
-    setup();
-    Callback_Config_t ConfigWithNoCallback;
-    ConfigWithNoCallback.expiry_time = 2;
-    ConfigWithNoCallback.callback = NULL;
-
-    ret = Register_Callback(&ConfigWithNoCallback);
-    EXPECT_EQ(100, ret);
-}
-
-TEST(CallbackTests, CallsTheRegisteredCallback)
-{
-    // init
-    setup();
-
-    // register a 10 second callback
-    Callback_Config_t tenSecondConfig;
-    tenSecondConfig.callback = &ten_second_cb;
-    tenSecondConfig.expiry_time = 10;
-    ret = Register_Callback(&tenSecondConfig);
-    EXPECT_EQ(ret, 0); // successful registration
-
-    // printf("ID: %d\n", tenSecondConfig.callback_ID);
-    //  printf("Registered Callback Count: %d\n\n", Callbacks_GetCallbackCount());
-
-    // call the fake rti 10 times to simulate 10 seconds
-    for (U8 t = 0; t < 10; t++)
+    for (int t = 0; t < numTimes; t++)
     {
         FakeRTI();
     }
-
-    // printf("System Count: %d\n\n", Callbacks_GetCount());
-
-    int expected = WATER_PLANTS;
-    int actual = get_current_state();
-
-    EXPECT_EQ(expected, actual);
 }
 
-TEST(CallbackTests, ShallProvideCallbackWithID)
+TEST(CallbackTests, Init_ShallInitializeToInitialValues)
 {
-    // init
-    setup();
-    Callbacks_Init();
+    /**Initial Values**/
+    /*
+    Callback = NULL
+    Count = 0
+    Callback_Count = 0
+    ID = 0
+    Expiry Time = 0
+    */
 
-    // when registering a new callback, the ID of the callback will be
-    // set as the index it is saved at.
+    setup(); // calls the Init
 
-    Callback_Config_t config;
-    config.callback = ten_second_cb;
-    config.expiry_time = 10;
-
-    ret = Register_Callback(&config);
-
-    EXPECT_EQ(ret, 0);
-    // printf("ID: %d\n", config.callback_ID);
-
-    U8 expected = 0;
-    U8 actual = config.callback_ID;
-    EXPECT_EQ(expected, actual);
-}
-
-TEST(CallbackTests, ShallRegisterNoMoreThanTenCallbacks)
-{
-    // init
-    setup();
-    Callback_Config_t config;
-    config.callback = twentyFiveSecondCB;
-    config.expiry_time = 25;
-
-    U8 current_registered_callbacks_count = Callbacks_GetCallbackCount();
-
-    // register 10 callbacks (MAX number of callbacks)
-    for (int i = 0; i < 10 - current_registered_callbacks_count; i++)
-    {
-        ret = Register_Callback(&config);
-        // printf("ID: %d\n", config.callback_ID);
-    }
-
-    EXPECT_EQ(ret, 0);
-}
-
-TEST(CallbackTests, ShallDeleteARegisteredCallback)
-{
-    // init
-    setup();
-    Callback_Config_t toDelete;
-    toDelete.callback = twentyFiveSecondCB;
-    toDelete.callback_ID = 7;
-    toDelete.expiry_time = 25;
-
-    ret = Delete_Callback(&toDelete);
-    EXPECT_EQ(ret, 0); // successful delete
-}
-
-TEST(CallbackTests, ShallGetCurrentlyRegisteredCallbacksCount)
-{
-    // init
-    setup();
-    U8 current_registered_callbacks_count = Callbacks_GetCallbackCount();
-    U8 expected = 9;
-    U8 actual = current_registered_callbacks_count;
-
-    EXPECT_EQ(expected, actual);
-}
-
-TEST(CallbackTests, InitShallSetAllSpacesToOpen)
-{
-    Callbacks_Init();
     U8 expected = 0;
     U8 actual = Callbacks_GetCallbackCount();
-
     EXPECT_EQ(expected, actual);
 }
 
-TEST(CallbackTests, ShallNotDeleteWhenNothingIsRegistered)
+TEST(CallbackTests, Register_ShallRegisterValidCallback)
 {
     // init
     setup();
-    Callback_Config_t toDelete;
-    toDelete.callback = twentyFiveSecondCB;
-    toDelete.callback_ID = 7;
-    toDelete.expiry_time = 25;
 
-    ret = Delete_Callback(&toDelete);
-    EXPECT_EQ(ret, 90); // unsuccessful delete
-}
-
-TEST(CallbackTests, ShallCallMultipleCallbacksSimultaneously)
-{
-    setup();
-    Callbacks_Init();
-
-    called_flag = 0;
-    twenty_fiveFlag = 0;
-
+    /**initialize a counter callback for 10 seconds*/
     Callback_Config_t config;
     config.callback = &ten_second_cb;
     config.expiry_time = 10;
 
-    Callback_Config_t config2;
-    config.callback = &twentyFiveSecondCB;
-    config.expiry_time = 25;
+    ret = Register_Callback(&config);
+    U8 success = 0;
+    EXPECT_EQ(success, ret);
+}
+
+TEST(CallbackTests, Register_ShallNOTRegisterWithNullCallback)
+{
+    // init
+    setup();
+
+    /**initialize a counter callback for 10 seconds*/
+    Callback_Config_t config;
+    config.callback = NULL;
+    config.expiry_time = 100;
 
     ret = Register_Callback(&config);
-    printf("ID: %d\n", config.callback_ID);
-    EXPECT_EQ(ret, 0); // successful registeration
+    U8 failure = 100;
+    EXPECT_EQ(failure, ret);
+}
 
-    ret = Register_Callback(&config2);
-    printf("ID: %d\n", config2.callback_ID);
-    EXPECT_EQ(ret, 0); // successful registeration
+TEST(CallbackTests, Register_ShallNOTRegisterWithLessThanOrEqualToZeroExpiry)
+{
+    // init
+    setup();
 
-    // config.callback();
-    // config2.callback();
+    Callback_Config_t config;
+    config.callback = &ten_second_cb;
+    config.expiry_time = 0; // invalid
 
-    printf("Registered: %d\n", Callbacks_GetCallbackCount());
+    ret = Register_Callback(&config);
+    U8 failure = 100;
+    EXPECT_EQ(failure, ret);
+}
 
-    for (int i = 0; i < 100; i++)
+TEST(CallbackTests, Register_ShallNOTRegisterMoreThanMAXCallbacks)
+{
+    // init
+    setup();
+
+    /**initialize a counter callback for 10 seconds*/
+    Callback_Config_t config;
+    config.callback = &ten_second_cb;
+    config.expiry_time = 10;
+
+    // registring MAX number of callbacks which is allowed
+    for (int i = 0; i < MAX_CALLBACKS; i++)
     {
-        // calling rti 100 times
-        // the ten second callback should be called 10 times
-        // the twenty five second  should be called 5 times
-        FakeRTI();
+        ret = Register_Callback(&config);
     }
 
-    printf("Count: %d\n", Callbacks_GetCount());
+    U8 success = 0;
+    EXPECT_EQ(success, ret);
 
-    EXPECT_EQ(10, called_flag);
-    EXPECT_EQ(4, twenty_fiveFlag); // 4*25 = 100
+    // registry already full, including another one - FAIL
+    ret = Register_Callback(&config);
+    U8 failure = 100;
+    EXPECT_EQ(failure, ret);
+}
+
+TEST(CallbackTests, CallbackCount_ShallReportAccurateValue)
+{
+    // init
+    setup();
+
+    // valid regisration after setup
+    Callback_Config_t config;
+    config.callback = &ten_second_cb;
+    config.expiry_time = 10;
+
+    ret = Register_Callback(&config);
+    U8 success = 0;
+    EXPECT_EQ(success, ret);
+
+    U8 expected = 1;
+    U8 actual = Callbacks_GetCallbackCount();
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(CallbackTests, Manager_ShallCallRegisteredCallback)
+{
+    // init
+    setup();
+
+    Callback_Config_t config;
+    config.callback = &ten_second_cb;
+    config.expiry_time = 10;
+    ret = Register_Callback(&config);
+    U8 success = 0;
+    EXPECT_EQ(success, ret);
+
+    // simulating RTI being called 10 times
+    CallRTI(10);
+
+    U8 expected = 1;
+    U8 actual = called_flag;
+
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(CallbackTests, Manager_ShallCallMultipleCallbacksSimultaneously)
+{
+    // init
+    setup();
+
+    // callback 1
+    Callback_Config_t tenSecond;
+    tenSecond.callback = &ten_second_cb;
+    tenSecond.expiry_time = 10;
+    ret = Register_Callback(&tenSecond);
+    U8 success = 0;
+    EXPECT_EQ(success, ret);
+
+    // callback 2
+    Callback_Config_t twentySeven;
+    twentySeven.callback = &twenty_seven_sec_cb;
+    twentySeven.expiry_time = 27;
+    ret = Register_Callback(&twentySeven);
+    success = 0;
+    EXPECT_EQ(success, ret);
+
+    CallRTI(130);
+
+    U8 TenSecCBexpected = 13;
+    U8 TenSecCBactual = called_flag;
+    EXPECT_EQ(TenSecCBexpected, TenSecCBactual);
+
+    U8 TwentySevenSecCBexpected = 4;
+    U8 TwentySevenCBactual = twenty_seven_called_flag;
+    EXPECT_EQ(TwentySevenSecCBexpected, TwentySevenCBactual);
 }
